a922d57a208bc147e3bfa59abed8f74124f73619
JearinAlvarado <110737046+JearinAlvarado@users.noreply.github.com>
2022-08-09 14:01:14 -0400

    Add files via upload

---
+++ Login/src/Config/PreparedStatement.java
@@ -0,0 +1,14 @@
+
+package Config;
+
+/**
+ *
+ * @author Jearin Alvarado
+ */
+class PreparedStatement {
+
+    void executeUpdate() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ Login/src/Config/Tabla.java
@@ -0,0 +1,34 @@
+
+package Config;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Jearin Alvarado
+ */
+public class Tabla {
+
+    static void setModel(DefaultTableModel modelo) {
+    }
+
+    static int getSelectedRow() {
+        return 0;
+    }
+
+    static Object getValueAt(int fila, int i) {
+        return null;
+    }
+
+    static int getRowCount() {
+        return 0;
+    }
+    
+    static void getModel(DefaultTableModel modelo) {
+    }
+
+    static int getSelectRow() {
+        return 0;
+    }
+    
+}
